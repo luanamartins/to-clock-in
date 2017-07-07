9a02bbed4b0fd10f2407f7b0812d7fca9c9fecea
Luana Martins <lms7@cin.ufpe.br>
2017-07-07 04:51:05 -0300

    Update script file to add users

--- src/main/resources/scripts/populate-users-table.sql
+++ src/main/resources/scripts/populate-users-table.sql
@@ -1,1 +1,10 @@
-INSERT INTO `workers` (`pis`, `name`) VALUES (<{pis: }>,<{name: }>);
+INSERT INTO `workers` (`pis`, `name`) VALUES ("12083924837", "nome1");
+INSERT INTO `workers` (`pis`, `name`) VALUES ("12055473249", "nome2");
+INSERT INTO `workers` (`pis`, `name`) VALUES ("12031314892", "nome3");
+INSERT INTO `workers` (`pis`, `name`) VALUES ("12089294380", "nome4");
+INSERT INTO `workers` (`pis`, `name`) VALUES ("12063955842", "nome5");
+INSERT INTO `workers` (`pis`, `name`) VALUES ("12007412855", "nome6");
+INSERT INTO `workers` (`pis`, `name`) VALUES ("12073641352", "nome7");
+INSERT INTO `workers` (`pis`, `name`) VALUES ("12081175233", "nome8");
+INSERT INTO `workers` (`pis`, `name`) VALUES ("12061634640", "nome9");
+INSERT INTO `workers` (`pis`, `name`) VALUES ("12063168140", "nome10");
